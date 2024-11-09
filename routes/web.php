cef6d9e2788906157c515431d56d35aa12e43c53
GabziDev <ggabz338972007@gmail.com>
2024-11-09 20:38:13 +0100

    Update

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/classement', [StatsAzuriomHomeController::class, 'index']);
+Route::get('/', [StatsAzuriomHomeController::class, 'index']);
